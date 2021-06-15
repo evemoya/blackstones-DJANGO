0c1fa6e3b48cc5fe74cb4ee0e1a68aecd0a85b1e
evemoya <eve.moya@duocuc.cl>
2021-06-15 05:35:11 -0400

    Subida Tienda

--- BS/urls.py
+++ BS/urls.py
@@ -4,13 +4,15 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import index, galeria, agendarhora, informaciones
+from .views import index, galeria, agendarhora, informaciones, tienda
 
 urlpatterns = [
     path ('', index, name='IND'),
     path ('gale/', galeria, name='GALE' ),
     path ('agenda/',agendarhora, name='AGENDARHORA'),
     path ('info/',informaciones, name='INFO'),
+    path ('info/',tienda, name='TIENDA'),
+
     
     
 ]    
